51fc20c68d5f04eb229c8b46be28001213127f46
Puguh Sudarmawanto <puguhsudarmawanto@gmail.com>
2021-03-25 02:17:12 +0700

    init laravel and bootstrap

---
+++ resources/views/layout/partials/head.blade.php
@@ -0,0 +1,32 @@
+<meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>OnePage Bootstrap Template - Index</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="OnePage/assets/img/favicon.png" rel="icon">
+  <link href="OnePage/assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="OnePage/assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="OnePage/assets/vendor/icofont/icofont.min.css" rel="stylesheet">
+  <link href="OnePage/assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="OnePage/assets/vendor/remixicon/remixicon.css" rel="stylesheet">
+  <link href="OnePage/assets/vendor/venobox/venobox.css" rel="stylesheet">
+  <link href="OnePage/assets/vendor/owl.carousel/assets/owl.carousel.min.css" rel="stylesheet">
+  <link href="OnePage/assets/vendor/aos/aos.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="OnePage/assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: OnePage - v2.2.2
+  * Template URL: https://bootstrapmade.com/onepage-multipurpose-bootstrap-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
