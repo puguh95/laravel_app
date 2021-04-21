8d42a4990114cfe778b7a91a94defed75c7cb8d5
Puguh Sudarmawanto <puguhsudarmawanto@gmail.com>
2021-04-21 21:15:56 +0700

    edit title

--- resources/views/layout/admin/register.blade.php
+++ resources/views/layout/admin/register.blade.php
@@ -26,7 +26,7 @@
   <div class="col-sm-12">
   <div class="card card-primary">
     <div class="card-header">
-      <h3 class="card-title">Edit User</h3>
+      <h3 class="card-title">Register Account</h3>
     </div>
     <!-- /.card-header -->
     <!-- form start -->
